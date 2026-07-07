28f10c80405b6b989b1ff41832d8dba8227e58f4
Nosratee Jahan Naba <187439614+soloflare1@users.noreply.github.com>
2026-07-08 02:57:34 +0600

    Create Constants + Type Casting.php

---
+++ lab 11/Constants + Type Casting.php
@@ -0,0 +1,17 @@
+<?php
+
+    define("SITE_NAME","Web Tech Lab");
+    const MAX_USERS = 100;
+    echo SITE_NAME."<br>";
+    echo "Maximum Users: ".MAX_USERS."<br>";
+
+    $str="42";
+    $num=(int)$str;
+    $flt=(float)"3.14";
+    $bool=(bool)0;
+
+    var_dump($num);
+    var_dump($flt);
+    var_dump($bool);
+
+?>
