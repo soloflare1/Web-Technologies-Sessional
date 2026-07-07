e2f0a19a038bff662884c3558f3923e573216915
Nosratee Jahan Naba <187439614+soloflare1@users.noreply.github.com>
2026-07-08 02:54:28 +0600

    Update Form.php

--- lab 11/Form.php
+++ lab 11/Form.php
@@ -1,7 +1,23 @@
-<form method="post" action="process.php">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form method="post" action="process.php">
+        <input type="text" name="username" placeholder="Enter name">
+        <input type="submit" value="Submit">
+    </form>
 
-<input type="text" name="username" placeholder="Enter name">
+    <?php
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $username = $_POST['username'];
+            echo "Hello, " . htmlspecialchars($username) . "!";
+        }
+    ?>
+</body>
+</html>
 
-<input type="submit" value="Submit">
 
-</form>
